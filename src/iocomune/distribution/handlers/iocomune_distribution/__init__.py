af31eddf9e3b8a6fd1f422e2892ffc15edefb38f
Roman <roman.kysil@redturtle.it>
2024-05-15 12:18:03 +0200

    IoComuneDistribution

--- src/iocomune/distribution/handlers/iocomune_distribution/__init__.py
+++ src/iocomune/distribution/handlers/iocomune_distribution/__init__.py
@@ -1,10 +1,17 @@
+from plone.distribution.handler import (
+    default_handler,
+    default_pre_handler,
+    post_handler as post_handler_orig,
+)
+
+
 def pre_handler(answers: dict) -> dict:
-    return answers
+    return default_pre_handler(answers)
 
 
 def handler(distribution, site, answers: dict):
-    return site
+    return default_handler(distribution, site, answers)
 
 
 def post_handler(distribution, site, answers: dict):
-    return site
+    return post_handler_orig(distribution, site, answers)
